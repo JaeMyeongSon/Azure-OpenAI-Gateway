e1438bdbcc856864248b7ac2bad4bb61b673e9a9
jasong <jasong@student.42seoul.kr>
2025-06-15 16:16:13 +0900

    feat: add nest swagger and add morgan for log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // decorator(@)가 없는 속성이 들어오면 해당 속성은 제거하고 받아들입니다.
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 값이 넘어오면 request 자체를 막습니다.
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('OpenAI Loadbalancer API')
+    .setDescription('API documentation for OpenAI Loadbalancer')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(parseInt(configService.get('PORT') || '3000'));
 }
 bootstrap();
